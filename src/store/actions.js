f72c46f6425ba400bf43ed3afbb968e723494173
Maksim Home <akiam@mail.ru>
2017-10-24 21:36:53 +0300

    Initial commit

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+export const  updateValue = (context, payload) => {
+    context.commit('updateValue', payload)
+};
+
+export const action2 = () => {};
+
---
+++ src/store/mutations.js
@@ -0,0 +1,3 @@
+const updateValue = (state, payload) => {
+    state.value = payload;
+};
